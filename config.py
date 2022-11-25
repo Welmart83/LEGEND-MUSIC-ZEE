f96fa90bb655326f53a696110dad06422363da05
Welmart83 <mzidan01010@gmail.com>
2022-11-25 20:26:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ load_dotenv()
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
-        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
-        self.SUDOERS: list = [
+        self.API_ID: 15638315
+        self.API_HASH: 1ab900848c1e5f3529f79f729e88b621
+        self.SESSION:  BQC3rIM0sBvLsR1_afGxY26i8OHUmgH3blZBcZaOO-6KlCtdJN_YgkGrgNGY6cSiPFTqlmi3kJpJqp3LsfT0wukQe_0U1c8VBHJiVZUHjB7f2NosuFSGuUZDwJzEEJYMKgvwxEiMAQKy-4UMG9ZpKgLBrQoUCt3L1HJkRBMSEzpbMIthbAnBfTmRN8SujNniqnOV0bbiRnAmh5GG1NHlqExNO1AOYzhTxTRJfG_mKGvKXZdQdaIOYbAQgHNIvH1mO6S_2MrVC_w9VwxtaewxRDDvdzrm2kf_LQld4H7hiu5EEFKw7kBSlkbls4YF-zHPGeid8suCGaw3krcd5acrOsxhfKUcwQA
+        self.BOT_TOKEN: 5245012700:AAFvOT_XmlL6n75rQb8aE0b04FwNt46sqbI
+        self.SUDOERS: list = 1992156749
             int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
         ]
         if not self.SESSION or not self.API_ID or not self.API_HASH:
